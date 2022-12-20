8983092360af374106ada818e38ee1f933a1909a
rzs <rasmus.norgaard@gmail.com>
2022-12-20 11:00:31 +0100

    Setup eslint config with autosave

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,45 @@
 #!/usr/bin/env ts-node
-"use strict";
+'use strict';
 
-import util from "util";
-import path from "path";
-import minimist from "minimist"
-import fs from "fs"
-import getStdin from "get-stdin"
+import util from 'util';
+import path from 'path';
+import minimist from 'minimist';
+import fs from 'fs';
+import getStdin from 'get-stdin';
 
 const args = minimist(process.argv.slice(2), {boolean: ['help', 'in'],string: ['file']});
 
 if (args.help) {
-    printHelp();
+  printHelp();
 } else if (args.in || args._.includes('-')) {
-    getStdin().then(processFile).catch(error);
+  getStdin().then(processFile).catch(error);
 } else if (args.file) {
-    fs.readFile(path.resolve(args.file), (err, contents) => {
-        if (err) {
-            error(err.toString());
-        } else {
-            processFile(contents.toString());
-        }
-    })
+  fs.readFile(path.resolve(args.file), (err, contents) => {
+    if (err) {
+      error(err.toString());
+    } else {      
+      processFile(contents.toString());
+    }
+  });
 } else {
-    error('In-correct usage!\n');
-    printHelp();
+  error('In-correct usage!\n');
+  printHelp();
 }
 
 function printHelp() {
-    console.log('ex1 usage:');
-    console.log('npx ts-node src/index.ts --file={FILENAME}');
-    console.log('');
-    console.log('--help                         print this help');
-    console.log('--file={FILENAME}              process the file');
-    console.log('--in, -                        process stdin');
-    console.log('');
+  console.log('ex1 usage:');
+  console.log('npx ts-node src/index.ts --file={FILENAME}');
+  console.log('');
+  console.log('--help                         print this help');
+  console.log('--file={FILENAME}              process the file');
+  console.log('--in, -                        process stdin');
+  console.log('');
 }
 
 function processFile(contents: string) {
-    process.stdout.write(contents);
+  process.stdout.write(contents);
 }
 
 function error(msg: string) {
-    console.error(msg);
+  console.error(msg);
 }
